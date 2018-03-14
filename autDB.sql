08b02480580532061004416b6aa540d907ae6421
a15wilhe <a15wilhe@student.his.se>
2018-03-14 11:56:24 +0100

    added db creation, user & tables for click & mousemove

---
+++ autDB.sql
@@ -0,0 +1,30 @@
+DROP DATABASE IF EXISTS autDB;
+CREATE DATABASE IF NOT EXISTS autDB;
+USE autDB;
+
+CREATE USER 'autUser'@'localhost' IDENTIFIED BY 'maninthemoon';
+GRANT ALL PRIVILEGES ON * . * TO 'autUser'@'localhost';
+FLUSH PRIVILEGES;
+
+
+/* ---- AUT DB #1----- */
+/*events - click, mouse, hover, tab, resize, scroll*//* add -- target */
+CREATE TABLE IF NOT EXISTS click_table (
+	logID INT AUTO_INCREMENT,	
+    href TEXT,
+	x INT,
+	y INT,
+	xscroll INT,
+	yscroll INT,
+	TIMESTAMP DATETIME DEFAULT CURRENT_TIMESTAMP,
+	PRIMARY KEY (logID)
+);
+
+CREATE TABLE IF NOT EXISTS mousemove_table (
+	logID INT AUTO_INCREMENT,
+	href TEXT,
+	mouseX TEXT,
+	mouseY TEXT,
+	TIMESTAMP DATETIME DEFAULT CURRENT_TIMESTAMP,
+	PRIMARY KEY (logID)
+);
