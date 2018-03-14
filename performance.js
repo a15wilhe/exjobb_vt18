eef7c46f28253d46f71420f3c21f943edb31fdc3
a15wilhe <a15wilhe@student.his.se>
2018-03-14 14:53:02 +0100

    fixed performance measure

--- performance.js
+++ performance.js
@@ -1,29 +1,17 @@
-window.addEventListener("load", function(){//maybe change to jQuery document.ready//calc time to load page
-     // end timer
+//calc time to load page
+$( document ).ready(function() {
     var endTime = (new Date).getTime();
-
-    // calculate difference in time//add to check that sS is not null
-    //var startTime = parseInt(sessionStorage.getItem("aut-start-time"));
-    var startTime = sessionStorage.getItem("aut-start-time");
-    
-    //var startTime = sessionStorage.getItem("aut-start-time");
-    
-    //if (startTime == undefined || startTime == null){}//do nothing if first time
-
-        var diffTime = endTime - startTime;
-        var page = window.location.href;
-
-        console.log(startTime);
-        console.log(diffTime);
-        //ajax to server
-        $.ajax({
-            type: 'POST',
-            url: "performance.php",
-            data: { performance: "time: " + diffTime + ", page: " + page }
-        });
+    var startTime = parseInt(sessionStorage.getItem("aut-start-time"));
+    var diffTime = endTime - startTime;
+    var page = window.location.href;
+    //ajax to server
+    $.ajax({
+        type: 'POST',
+        url: "performance.php",
+        data: { performance: "time: " + diffTime + ", page: " + page }
+    });
 });
-
+//Start timer on unload - user leaves a page
 window.addEventListener("unload", function(){
-    //localStorage.setItem("autstarttime", (new Date).getTime().toString());// start timer when leaving
     sessionStorage.setItem("aut-start-time", (new Date).getTime().toString());
 });
